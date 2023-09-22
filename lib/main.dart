65195469e21aa6adc6cda0b3ef90dc70f36edf6a
flucloxin500 <mdwalid826@gmail.com>
2023-09-23 01:02:41 +0600

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -109,11 +109,104 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: HomeScreen(),
+      theme: ThemeData(
+        primaryColor: Colors.green,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.white,
+                backgroundColor: Colors.green,
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8)),
+                elevation: 5,
+                textStyle: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 0.6,
+                  wordSpacing: 0.5,
+                ))),
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            textStyle: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+            ),
+            foregroundColor: Colors.deepOrange,
+          ),
+        ),
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.purple,
+          elevation: 5,
+          shadowColor: Colors.green,
+        ),
+        textTheme: TextTheme(
+          bodyMedium: TextStyle(
+            fontSize: 18,
+          ),
+          bodySmall: TextStyle(fontSize: 12),
+          bodyLarge: TextStyle(fontSize: 22),
+        ),
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.amberAccent,
+          elevation: 5,
+          shadowColor: Color(0xFFF082FF),
+        )
+      ),
+      themeMode: ThemeMode.dark,
     );
   }
 }
 
 class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Hello World!'),
+            Text(
+              'Hello World!',
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+            Text('Hello World!'),
+            TextButton(
+              onPressed: () {},
+              child: Text('Tap here'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Tap here'),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red,
+                ),
+                onPressed: () {},
+                child: Text('Tap here'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/// Navigator from one route to another
+
+/*class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
@@ -305,3 +398,5 @@ class ProductDetailsList extends StatelessWidget {
         ));
   }
 }
+
+ */
