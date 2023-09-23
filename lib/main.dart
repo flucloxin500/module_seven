98a10a3df6b235635aa3136aa903b391f49bc357
flucloxin500 <mdwalid826@gmail.com>
2023-09-24 01:49:36 +0600

    Basic Cupertino widget has been added, also added alert dialog with material app

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,26 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main (){
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp ({super.key}) ;
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoApp(
-      home: Home() ,
+    return MaterialApp(
+      home: Home(),
     );
   }
 }
 
-class Home extends StatelessWidget{
-  const Home ({super.key}) ;
+class Home extends StatelessWidget {
+  const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
+    /*
     return CupertinoPageScaffold(
       navigationBar: CupertinoNavigationBar(
         leading: Icon(CupertinoIcons.home),
@@ -52,7 +52,83 @@ class Home extends StatelessWidget{
           ])
         ],
       ),
-    )
+    ),
+    );
+  }
+     */
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Column(
+        children: [
+          // Switch
+          Switch(value: true, onChanged: (bool value) {}),
+          // alert box
+          ElevatedButton(
+            onPressed: () {
+              showDialog(
+                  barrierDismissible: true,
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: Text('Alert'),
+                      content: Text('You are in danger.!'),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            // hiding with cancel button
+                            Navigator.pop(context);
+                          },
+                          child: Text('Cancel'),
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          child: Text('Okay'),
+                        ),
+                      ],
+                    );
+                  });
+            },
+            child: Text('Show dialog'),
+          ),
+          // show dialg
+          ElevatedButton(onPressed: (){
+            showAboutDialog(context: context ,
+                applicationName: "Pikapie",
+            applicationVersion: '1.04.20',
+            children: [
+              Text('This is good'),
+            ]);
+          }, child: Text('Show About'),),
+          // bottom sheet
+          ElevatedButton(onPressed: (){
+            showModalBottomSheet(
+              isDismissible: false,
+                barrierColor: Colors.black45,
+                backgroundColor: Colors.grey.shade50,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(16),
+                    topRight: Radius.circular(16),
+                  )
+                ),
+                context: context, builder: (context){
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Center(
+                      child: Text('This is bottom sheet'),
+                    )
+                  ],
+                ),
+              );
+            });
+          }, child: Text('Show bottom sheet')),
+        ],
+      ),
     );
   }
 }
