7dae6ad153b153d6cb0d7251ae5b58ab99a468ac
flucloxin500 <mdwalid826@gmail.com>
2023-09-25 23:25:46 +0600

    Counter App, set State and widget

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,52 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+// Immutable/ Statefull widget
+
+/*class HomeScreen extends StatelessWidget{
+  HomeScreen ({super.key});
+
+  int counter = 0 ;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Text(counter.toString(),style: Theme.of(context).textTheme.displayLarge,),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          counter = counter + 1 ;
+          print(counter);
+        },
+        child: Icon(Icons.add),
+      ),
+    ) ;
+  }
+
+}*/
+/*
+
+// Immutable widget
+
+class HomeScreen extends StatelessWidget{
+  const HomeScreen ({super.key});
+
+  static const String name = 'Rafat' ;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold() ;
+  }
+
+}
+
+ */
+// Mutable?stateful
 class CounterScreen extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -28,77 +74,70 @@ class CounterState extends State<CounterScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Counter App'),
+        title: Text('Home'),
       ),
       body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Count:'),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 10),
-                child: Text(counter.toString(),
-                    style: TextStyle(
-                      fontSize: 55,
-                      fontWeight: FontWeight.bold,
-                    ),
-                ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              counter.toString(),
+              style: TextStyle(
+                fontSize: 45,
+                fontWeight: FontWeight.bold,
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(onPressed: (){
-                    setState(() {
-                      if ( counter < 5)
-                        {
-                          counter = counter + 1;
-                        }
-                      else
-                        {
-                          showDialog(context: context, builder: (context){
-                            return AlertDialog(
-                              title: Text('Button Pressed ${counter} times',
-                              style: TextStyle(
-                                fontWeight: FontWeight.normal
-                              ),),
-                              actions: [
-                                TextButton(onPressed: (){},
-                                    child: Text('Close'),),
-                              ],
-                            );
-                          }
-                          );
-                        }
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              ProfileScreen(userName: 'Snake CM')));
+                },
+                child: Text('Profile Screen'))
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // if ( counter < 10)
+          //   {
+          //     counter = counter + 1;
+          //
+          //   }
+          // You can count it here tooo
+          counter = counter + 1;
+          setState(() {
+            //counter = counter + 1;
+          });
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
 
-                    });
-                  },
-                      child: Text('+',
-                      style: TextStyle(
-                        fontSize: 25
-                      ),),),
+// Widget Class
+class ProfileScreen extends StatefulWidget {
+  final String userName;
+  const ProfileScreen({super.key, required this.userName});
+  @override
+  State<StatefulWidget> createState() {
+    return _ProfileScreenState();
+  }
+}
 
-                  Padding(
-                    padding: const EdgeInsets.only(left: 18),
-                    child: ElevatedButton(onPressed: (){
-
-                      setState(() {
-                        if ( counter > 0)
-                          {
-                            counter = counter - 1;
-                          }
-
-                      });
-                    },
-                      child: Text('-',
-                      style: TextStyle(
-                        fontSize: 25
-                      ),),),
-                  ),
-                ],
-              )
-            ],
-          ),
-
+// State Class -> widget class
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile'),
+      ),
+      body: Center(
+        child: Text(widget.userName),
       ),
     );
   }
